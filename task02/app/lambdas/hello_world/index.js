77451c7b837155e549eb3350816ae674b0ed2774
mateozapata <mateozacar@gmail.com>
2025-01-28 10:08:26 -0500

    lambda sydicate

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,4 +1,16 @@
 exports.handler = async (event) => {
+
+    const expectedPath = '/hello';
+     if (event.path !== expectedPath) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: `Bad request syntax or unsupported method. Request path:  ${event.path}. HTTP method: ${event.httpMethod}`,
+                    statusCode: 400  // Including status code in the body if required
+                })
+            };
+      }
+
     const response = {
         statusCode: 200,
         body: JSON.stringify({
