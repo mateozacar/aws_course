36841454679e4040479c19949e95f697150a5200
mateozapata <mateozacar@gmail.com>
2025-01-28 10:05:05 -0500

    lambda sydicate

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,28 +1,10 @@
 exports.handler = async (event) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-
-    // Assuming GET is the only supported method and /cmtr-27d20cfd is the valid path.
-    const validPath = '/hello';
-
-    // Check for the HTTP method and path
-    if (event.path !== validPath) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                statusCode: 400,
-                message: 'Bad request syntax or unsupported method. Request path: ${event.path}. HTTP method: ${event.httpMethod}'
-            })
-        };
-    }
-
-    // If the method is GET and path is correct, process the request
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            statusCode: 200,
+            statusCode: 200,    // Including status code inside the body
             message: 'Hello from Lambda'
         })
     };
-
     return response;
 };
